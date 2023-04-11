1e5103066ee4ca122dd3a5186d0c673f45d300d8
Weber-Paulo <paulomorandini@gmail.com>
2023-04-11 20:45:12 -0300

    Seção 03 - Exercício 05

---
+++ CodigoFonte/secao03-exercicio05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	//Declarando variáveis
+	int metros, centimetros;
+
+	//Entrada
+	printf("Informe o valor em metros: ");
+	fflush(stdout);
+	scanf("%d", &metros);
+
+	//Processamento
+	centimetros = (metros * 100);
+
+	//Saída
+	printf("%d metros em centímetros é %d", metros, centimetros);
+}
