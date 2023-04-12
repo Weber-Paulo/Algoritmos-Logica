08621b58225514f0b86ac7ff448c77656587dbc9
Weber-Paulo <paulomorandini@gmail.com>
2023-04-11 21:12:11 -0300

    Seção 03 - Exercício 07

---
+++ CodigoFonte/secao03-exercicio07.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	//Declarando variáveis
+	float altura, peso_ideal;
+
+	//Entrada
+	printf("Qual sua altura? ");
+	fflush(stdout);
+	scanf("%f", &altura);
+
+	//Processamento
+	peso_ideal = (72.7 * altura) - 58;
+
+	//Saída
+	printf("Seu peso ideal seria %.2f", peso_ideal);
+}
