f5f1823b7d32c31ce5ab7de7cf94fcf5640af521
Weber-Paulo <paulomorandini@gmail.com>
2023-05-07 21:51:46 -0300

    Seção 08 - Exercício 06

---
+++ CodigoFonte/secao08/secao08-exercicio06.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	//variaveis
+	float vetor[5];
+	int codigo;
+
+	//entradas
+	printf("Informe o código: ");
+	fflush(stdout);
+	scanf("%d", &codigo);
+	if(codigo != 0){
+		for(int i = 0; i < 5; i++){
+			//entradas
+			printf("Informe um número real: ");
+			fflush(stdout);
+			scanf("%f", &vetor[i]);
+		}
+		//processamento
+		if(codigo == 1){
+			for(int i = 0; i < 5; i++){
+				printf("%.2f\n", vetor[i]);
+			}
+		}
+		if(codigo == 2){
+			for(int i = 4; i >= 0; i--){
+				printf("%.2f\n", vetor[i]);
+			}
+		}
+	}
+}
