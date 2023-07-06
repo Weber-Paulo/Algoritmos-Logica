d5ff477b89c8b2a564bf1978294e183d5d7b226a
Weber-Paulo <paulomorandini@gmail.com>
2023-07-05 21:19:20 -0300

    Seção 06 - Exercício 02

---
+++ ProgramasPython/secao06/Exercicio02.py
@@ -0,0 +1,10 @@
+#entrada
+numero = int(input("Informe um número: "))
+#processamento
+if numero > 0:
+    a = numero
+    print("Valor positivo")
+else:
+    b = numero
+    print("Valor negativo")
+print(numero)
